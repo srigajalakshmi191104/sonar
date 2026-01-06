d260d720b64a7f9789890f23bdbb4e4de3113898
srigajalakshmi191104 <srigajalakshmi191104@gmail.com>
2026-01-05 23:51:00 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,36 +3,39 @@ import sqlite3
 
 API_KEY = "sk-test-1234567890"
 
+
 def get_user(username):
+
     conn = sqlite3.connect("users.db")
     cursor = conn.cursor()
 
-    query = f"SELECT * FROM users WHERE username = '{username}'"
+    query = "SELECT * FROM users WHERE username = '" + username + "'"
     cursor.execute(query)
 
     return cursor.fetchall()
 
 
+def divide(a, b):
+    """
+    Sonar type: BUG
+    """
+    return a / b
+
+
 def run_command(user_input):
+
     os.system("echo " + user_input)
 
 
 def calculate(expression):
+
     return eval(expression)
 
 
-def divide(a, b):
-    return a / b
-
-
 if __name__ == "__main__":
-    name = input("Enter username: ")
-    print(get_user(name))
-
-    cmd = input("Enter command: ")
-    run_command(cmd)
-
-    exp = input("Enter math expression: ")
-    print(calculate(exp))
+    print(get_user("admin' OR '1'='1"))
 
     print(divide(10, 0))
+
+    run_command("hello")
+    print(calculate("2 + 2"))
